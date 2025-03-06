78d76461ca975c4192a3c1d04c5d4853637990e6
ggumburashvili <ggumburashvili@velitech.com>
2025-03-07 00:01:05 +0400

    added registering enums for gql

--- src/generator/generate-enums.ts
+++ src/generator/generate-enums.ts
@@ -6,12 +6,17 @@ export const generateEnums = (enumModels: DMMF.DatamodelEnum[]) => `
 ${each(
   enumModels,
   (model) => {
-    return `
+    return `import { registerEnumType } from "@nestjs/graphql";
+
 export const ${camel(model.name)} = [${each(model.values, (v) => `'${v.name}'`, ', ')}] as const;
 
 export enum ${pascal(model.name)} {
   ${each(model.values, (v) => `${v.name.toUpperCase()} = '${v.name.toUpperCase()}'\n`, ', ')}
 }
+
+registerEnumType(pascal(model.name), {
+    name: "${pascal(model.name)}",
+});
 `;
   },
   '\n',
